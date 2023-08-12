aed58edae13ed6b456258bb23a4711e4158173ad
Adnan-ju <adnan.jucse@gmail.com>
2023-08-12 22:10:24 +0600

    teacherlogin system added using javascript

---
+++ script.js
@@ -0,0 +1,136 @@
+function auth()
+{
+    var id = document.getElementById("id").value;
+    var password = document.getElementById("password").value;
+    // ishrat madam 
+    if(id=="ishratjahan" && password=="ij2023")
+    {
+        window.location.assign("TeachersProfile/isratmadam.html");
+        alert("Login Successfully");
+    }
+    //abusayedsir
+    else if(id=="abusayed" && password=="asmmr2023")
+    {
+        window.location.assign("TeachersProfile/abusayedsir.html");
+        alert("Login Successfully");
+    }
+    //aminamadam 
+    else if(id=="aminakhatun" && password=="ak2023")
+    {
+        window.location.assign("TeachersProfile/aminamadam.html");
+        alert("Login Successfully");
+    }
+    //anupsir
+    else if(id=="anupmajumder" && password=="am2023")
+    {
+        window.location.assign("TeachersProfile/anupsir.html");
+        alert("Login Successfully");
+    }
+    //bulbulsir
+    else if(id=="bulbulahammad" && password=="ba2023")
+    {
+        window.location.assign("TeachersProfile/bulbulsir.html");
+        alert("Login Successfully");
+    }
+    //ezharsir
+    else if(id=="ezharulislam" && password=="ei2023")
+    {
+        window.location.assign("TeachersProfile/isratmadam.html");
+        alert("Login Successfully");
+    }
+    //hanifsir
+    else if(id=="hanifali" && password=="ha2023")
+    {
+        window.location.assign("TeachersProfile/hanifsir.html");
+        alert("Login Successfully");
+    }
+    //humayansir
+    else if(id=="humayunkabir" && password=="hk2023")
+    {
+        window.location.assign("TeachersProfile/humayansir.html");
+        alert("Login Successfully");
+    }
+    //imdad sir
+    else if(id=="imdadulislam" && password=="ii2023")
+    {
+        window.location.assign("TeachersProfile/imdadsir.html");
+        alert("Login Successfully");
+    }
+    //jugal sir
+    else if(id=="jugalkrishnadas" && password=="jkd2023")
+    {
+        window.location.assign("TeachersProfile/jugalsir.html");
+        alert("Login Successfully");
+    }
+    //kalamsir
+    else if(id=="abulkalamazad" && password=="aka2023")
+    {
+        window.location.assign("TeachersProfile/kalamsir.html");
+        alert("Login Successfully");
+    }
+    //Liton sir
+    else if(id=="litonzuderozario" && password=="lzr2023")
+    {
+        window.location.assign("TeachersProfile/litonsir.html");
+        alert("Login Successfully");
+    }
+    //Moazzam sir
+    else if(id=="golammoazzam" && password=="gm2023")
+    {
+        window.location.assign("TeachersProfile/moazzemsir.html");
+        alert("Login Successfully");
+    }
+    //morium madam
+    else if(id=="moriumakter" && password=="ma2023")
+    {
+        window.location.assign("TeachersProfile/moriummadam.html");
+        alert("Login Successfully");
+    }
+    //Rafsan Sir
+    else if(id=="rafsanjani" && password=="rj2023")
+    {
+        window.location.assign("TeachersProfile/rafsansir.html");
+        alert("Login Successfully");
+    }
+    //ritu madam
+    else if(id=="nadiaafrinritu" && password=="nar2023")
+    {
+        window.location.assign("TeachersProfile/ritumadam.html");
+        alert("Login Successfully");
+    }
+    //sabrina madam
+    else if(id=="sabrinasharmin" && password=="ss2023")
+    {
+        window.location.assign("TeachersProfile/sabrinamadam.html");
+        alert("Login Successfully");
+    }
+    //sarnalimadam
+    else if(id=="sarnalibasak" && password=="sb2023")
+    {
+        window.location.assign("TeachersProfile/sarnalimadam.html");
+        alert("Login Successfully");
+    }
+    //shorifsir
+    else if(id=="shorifuddin" && password=="msu2023")
+    {
+        window.location.assign("TeachersProfile/shorifsir.html");
+        alert("Login Successfully");
+    }
+    //sovon sir
+    else if(id=="ashrafulislam" && password=="ais2023")
+    {
+        window.location.assign("TeachersProfile/sovonsir.html");
+        alert("Login Successfully");
+    }
+    //Zahid sir
+    else if(id=="zahidurrahman" && password=="zr2023")
+    {
+        window.location.assign("TeachersProfile/zahidsir.html");
+        alert("Login Successfully");
+    }
+    else
+    {
+        alert("Invalid Information");
+        return;
+    }
+}
